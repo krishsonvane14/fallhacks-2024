dc23787927fc4ac220df036ffbe99ab9f025201f
Deepjot Grewal <deepjotgrewal50@gmail.com>
2024-09-28 14:28:47 -0700

    Starry background box created

--- three.js
+++ three.js
@@ -1,10 +1,6 @@
 import * as THREE from "three";
-import top from "./images/row-1-column-2.png";
-import left from "./images/row-2-column-1.png";
-import front from "./images/row-2-column-2.png";
-import right from "./images/row-2-column-3.png";
-import back from "./images/row-2-column-4.png";
-import bottom from "./images/row-3-column-2.png";
+import stars from "./images/stars.jpg";
+
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const renderer = new THREE.WebGLRenderer();
@@ -24,17 +20,19 @@ const camera = new THREE.PerspectiveCamera(
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 
 scene.background = cubeTextureLoader.load([
-  right,
-  left,
-  top,
-  bottom,
-  front,
-  back,
+  stars,
+  stars,
+  stars,
+  stars,
+  stars,
+  stars,
 ]);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 camera.position.set(-90, 140, 140);
 orbit.update();
+
+
 
 function animate() {
   renderer.render(scene, camera);
